ccb91526c9766f8ad199da9143aa1ae0cbfbe427
Sowon Choi <68233618+bee-p@users.noreply.github.com>
2025-01-24 23:37:04 +0900

    [level 1] Title: 폰켓몬, Time: 9.09 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 10.98 ms
+메모리: 87.4 MB, 시간: 9.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 23:33:50
+2025년 01월 24일 23:37:03
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,18 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
+        // 기존 사이즈 저장
         int size = nums.length;
         
         // 중복 제거
         nums = Arrays.stream(nums).distinct().toArray();
         
-        if (nums.length >= size / 2) {
-            answer = size / 2;
-        } else {
-            answer = nums.length;
-        }
 
-        return answer;
+        return nums.length >= (size / 2) ? size / 2 : nums.length;
     }
 }
