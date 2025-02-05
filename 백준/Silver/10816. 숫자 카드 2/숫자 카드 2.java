14b7eddd08ff325772d5b38279ab7a53581c03f2
Sowon Choi <68233618+bee-p@users.noreply.github.com>
2025-02-06 03:11:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1060 ms, Memory: 189960 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -4,70 +4,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
-    static int[] cardList;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // 데이터 입력받기
         int n = Integer.parseInt(br.readLine());
-        cardList = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        String[] cardStr = br.readLine().split(" ");
+
+        // key: 숫자, value: 숫자가 등장한 횟수
+        HashMap<Integer, Integer> cardMap = new HashMap<>();
+        for (String card : cardStr) {
+            int cardInt = Integer.parseInt(card);
+            cardMap.put(cardInt, cardMap.getOrDefault(cardInt, 0) + 1);
+        }
 
         int m = Integer.parseInt(br.readLine());
         int[] targetList = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        // 정렬 후 탐색
-        Arrays.sort(cardList);
+        // 해당 값 가져오기
         for (int i = 0; i < m; i++) {
-            int target = targetList[i];
-            bw.write(findUpperIndex(target) - findLowerIndex(target) + " ");
+            bw.write(cardMap.getOrDefault(targetList[i], 0) + " ");
         }
 
         bw.flush();
         bw.close();
-    }
-
-    // 탐색범위 : start <= x < end
-    static int findLowerIndex(int key) {
-        int start = 0;
-        int end = cardList.length;
-
-        // start == end면 탐색 종료
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if (key <= cardList[mid]) {
-                end = mid;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        return start;
-    }
-
-    // 탐색범위 : start <= x < end
-    static int findUpperIndex(int key) {
-        int start = 0;
-        int end = cardList.length;
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if (cardList[mid] <= key) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-
-        return end;
     }
 }
