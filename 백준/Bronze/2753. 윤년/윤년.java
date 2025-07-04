5c629c6c1faeec000c0c59ae58d064d12e3992d1
Sowon Choi <68233618+bee-p@users.noreply.github.com>
2025-07-04 21:08:19 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 연도 입력받기
+        int year = Integer.parseInt(br.readLine());
+
+        // 윤년일 때
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
