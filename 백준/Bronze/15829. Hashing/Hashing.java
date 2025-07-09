1e985f8e822677bbb0123ff18a928ae889919327
Sowon Choi <68233618+bee-p@users.noreply.github.com>
2025-07-09 10:34:04 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14644 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,11 +15,15 @@ public class Main {
         String input = br.readLine();
 
         // 해시 값 계산
-        int result = 0;
+        BigInteger result = BigInteger.ZERO;
         for (int i = 0; i < size; i++) {
-            result += (input.charAt(i) - 'a' + 1) * Math.pow(31, i);
+            BigInteger temp = BigInteger.ONE;
+            for (int k = 0; k < i; k++) {
+                temp = temp.multiply(BigInteger.valueOf(31));
+            }
+            result = result.add(temp.multiply(BigInteger.valueOf(input.charAt(i) - 'a' + 1)));
         }
-        result %= 1234567891;
+        result = result.mod(BigInteger.valueOf(1234567891));
 
         // 결과 출력
         bw.write("" + result);
