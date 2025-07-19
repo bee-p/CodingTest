67aeef872988d28ce239e021a79f8628f35a4c48
Sowon Choi <68233618+bee-p@users.noreply.github.com>
2025-07-19 16:26:27 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 780 ms, Memory: 59548 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(input[0]);
+            arr[i][1] = Integer.parseInt(input[1]);
+        }
+
+        Arrays.sort(arr, (a1, a2) -> {
+            if (a1[1] == a2[1]) {
+                // x 좌표가 증가하는 순서로
+                return a1[0] - a2[0];
+            } else {
+                // y 좌표가 증가하는 순서로
+                return a1[1] - a2[1];
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
