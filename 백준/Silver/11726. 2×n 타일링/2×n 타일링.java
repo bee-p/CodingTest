45dd4028ef8c707abcf9500cf4062554e354cd63
Sowon Choi <68233618+bee-p@users.noreply.github.com>
2025-08-01 22:48:05 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        // dp[i] : 2 x i 크기의 직사각형을 채우는 방법의 수를 담음
+        // dp[0]은 사용X
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        if (n >= 2) dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
+
